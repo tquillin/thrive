53c71d3d2fef8f9ca2a5c9cb9b52426be2bb7b9a
Terrell Quillin <terrellq@gmail.com>
2016-05-18 15:43:33 -0600

    Updates repo and has functioning login system.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -49,6 +49,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     end
 
+    test  "login with remembering" do
+      log_in_as(@user, remember_me: '1')
+      assert_not_nil cookies['remember_token']
+    end
 
+    test "login without remembering" do
+      log_in_as(@user, remember_me: '0')
+      assert_nil cookies['remember_token']
+    end 
 
 end
