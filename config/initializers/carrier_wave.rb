ba0fff3ebd49647e5f5ca00451c039317af4a345
Terrell Quillin <terrellq@gmail.com>
2016-05-24 13:17:54 -0600

    Updates repo.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
       # :region => ENV['S3_REGION']
     }
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = ENV['S3_MUSIC_LIFE']
   end
 end
 #Configuration for Amazon S3
