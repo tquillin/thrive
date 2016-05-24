4e2567911cf946a454764236986034a4a348007b
Terrell Quillin <terrellq@gmail.com>
2016-05-24 13:07:13 -0600

    Updates repo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,15 @@ end
 def password_reset_expired?
   reset_sent_at < 2.hours.ago
 end
+
+  # Defines a proto-feed
+  # See "Following users" for the full implementation.
+  def feed
+      Micropost.where("user_id = ?", id)
+  end
+
+
+
 private
   # Converts email to all lower-case.
   def downcase_email
