7a11d311261b9a54d76cfe988ab9a2c66e03cb7f
Terrell Quillin <terrellq@gmail.com>
2016-05-25 14:49:38 -0600

    updates repo.

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destroy]
 end
+# resources :relationships, only: [:create, :destroy]
 
 
 # resources :user
