26634525dcd683d4335a53c4d42df80997e5735a
Terrell Quillin <terrellq@gmail.com>
2016-08-15 18:09:33 -0600

    updates repo.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,9 +22,7 @@ class ActiveSupport::TestCase
       post login_path, session: { email: user.email, password: password, remember_me: remember_me }
     else
       session[:user_id] = user.id
-
     end
-
   end
 
   private
