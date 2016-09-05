5a09110a571d3d37095ece901c57e6ced9e2da00
Terrell Quillin <terrellq@gmail.com>
2016-09-05 14:24:35 -0600

    Updates repo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :microposts, dependent: :destroy 
+  has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
